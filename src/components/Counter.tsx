0637f19cf3a8f19ccbe60686d97221efb1444337
João Carlos Magalhães <joaocarlos.m014@gmail.com>
2023-01-27 13:11:07 -0300

    feat: use memo to reduce computational resources usage

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,10 @@
-import { createEffect, createSignal } from "solid-js"
+import { createEffect, createMemo, createSignal } from "solid-js"
+
+function fibonacci(num: number) {
+  if (num <= 1) return 1
+
+  return fibonacci(num - 1) + fibonacci(num - 2)
+}
 
 export default function Counter() {
   const [count, setCount] = createSignal(0)
@@ -6,6 +12,13 @@ export default function Counter() {
   createEffect(() => {
     // Show the counter only when it's divisible by 3
     if (count() % 3 === 0) console.log(count())
+  })
+
+  const fibMemo = createMemo(() => {
+    // Stop the calculation after 10 to prevent freezing the browser
+    console.log("called")
+    if (count() > 10) return `Limit reached, stopping for performance reasons`
+    return fibonacci(count())
   })
 
   const doubleCount = () => count() * 2
@@ -16,6 +29,7 @@ export default function Counter() {
     <div>
       <h1>This counter will increase after each second: {count() ?? `0`}</h1>
       <h1>The double of the counter is: {doubleCount ?? `0`}</h1>
+      <h1>The fibonacci of the counter is: {fibMemo() ?? `0`}</h1>
     </div>
   )
 }
