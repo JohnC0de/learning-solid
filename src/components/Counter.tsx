7a1d100a4c4566e6b648437398b2b6ceb5451b16
João Carlos Magalhães <joaocarlos.m014@gmail.com>
2023-01-27 12:29:32 -0300

    feat: show counter update on console with an effect

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,7 +1,11 @@
-import { createSignal } from "solid-js"
+import { createEffect, createSignal } from "solid-js"
 
 export default function Counter() {
   const [count, setCount] = createSignal(0)
+
+  createEffect(() => {
+    console.log(`Counter has been updated to ${count()}`)
+  })
 
   setInterval(() => setCount(count() + 1), 1000)
 
