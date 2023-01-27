5575adf461f59b669d7e1482fed9985e0c734d4f
João Carlos Magalhães <joaocarlos.m014@gmail.com>
2023-01-27 12:43:10 -0300

    feat: show the double of count utilizing Derived Signals

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,10 +4,18 @@ export default function Counter() {
   const [count, setCount] = createSignal(0)
 
   createEffect(() => {
-    console.log(`Counter has been updated to ${count()}`)
+    // Show the counter only when it's divisible by 3
+    if (count() % 3 === 0) console.log(count())
   })
+
+  const doubleCount = () => count() * 2
 
   setInterval(() => setCount(count() + 1), 1000)
 
-  return <div>This counter will increase after each second: {count() ?? `0`}</div>
+  return (
+    <div>
+      <h1>This counter will increase after each second: {count() ?? `0`}</h1>
+      <h1>The double of the counter is: {count() * 2 ?? `0`}</h1>
+    </div>
+  )
 }
