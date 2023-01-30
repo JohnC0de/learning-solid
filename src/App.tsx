284b7fba0f9549a8c137668be211b9b6030e5082
João Carlos Magalhães <joaocarlos.m014@gmail.com>
2023-01-30 15:22:39 -0300

    feat: use all components on main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,41 @@
-import { Component, createSignal, Show } from "solid-js"
-import CounterDisplay from "./components/CounterDisplay"
+import { Component, Show, Signal } from "solid-js"
+import CounterDisplay from "./components/Counter/CounterDisplay"
 import Divider from "./components/Divider"
-import LogButton from "./components/LogButton"
-import { useAuth } from "./context/auth"
+import TabButton from "./components/Tab/TabButton"
+import TodoDisplay from "./components/Todo/TodoDisplay"
+import { useGlobalContext } from "./context/global"
 
-export const btnStyle = "p-2 bg-blue-500 text-white rounded-xl"
-
-const App: Component = () => {
-  const { isLogged } = useAuth
-  const [showCounter, setShowCounter] = createSignal(false)
-  const counterState = { showCounter, setShowCounter }
+const LoginButton: Component<{ signal: Signal<boolean> }> = ({ signal }) => {
+  const [isLogged] = signal
 
   return (
-    <main class="flex flex-col h-screen mx-auto justify-center items-center w-300px">
-      <div class="border flex flex-col bg-zinc-100 rounded-2xl shadow-inner text-center p-4 gap-4 w-80">
-        <h1 class=" text-2xl">Hello, world!</h1>
+    <Show when={isLogged()} fallback={<TabButton signal={signal} text={"Log In ✅"} />}>
+      <TabButton signal={signal} text={"Log Out ❌"} />
+    </Show>
+  )
+}
+
+const App: Component = () => {
+  const { isLoggedSignal } = useGlobalContext
+  const [isLogged] = isLoggedSignal
+
+  return (
+    <main class="flex flex-col h-screen mx-auto w-300px justify-center items-center">
+      <div class="border flex flex-col bg-zinc-100 rounded-2xl shadow-inner text-center p-4 w-80 gap-4">
+        <h1 class="font-medium tracking-tighter text-zinc-600 text-3xl">Learning Solid</h1>
 
         <Divider />
 
-        <Show when={isLogged()} fallback={<LogButton text="Log In ✅" />}>
-          <LogButton text="Log Out ❌" />
-        </Show>
+        <LoginButton signal={isLoggedSignal} />
 
         <Divider />
 
         <Show when={isLogged()}>
-          <CounterDisplay {...counterState} />
+          <CounterDisplay />
+
+          <Divider />
+
+          <TodoDisplay />
         </Show>
       </div>
     </main>
