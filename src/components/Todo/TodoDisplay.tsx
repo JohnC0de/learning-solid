a751ac5b7f6f48a42030eb2ecd45e03349eeea9f
João Carlos Magalhães <joaocarlos.m014@gmail.com>
2023-01-31 01:45:02 -0300

    feat: add auto animate

--- src/components/Todo/TodoDisplay.tsx
+++ src/components/Todo/TodoDisplay.tsx
@@ -1,3 +1,4 @@
+import { createAutoAnimateDirective } from "@formkit/auto-animate/solid"
 import { Component, For } from "solid-js"
 import { useGlobalContext } from "../../context/global"
 import TabContainer from "../Tab/TabContainer"
@@ -10,11 +11,12 @@ const TodoDisplay: Component = () => {
 
   const addTodo = (text: string) => setTodos([...todos, { id: Date.now(), text, isCompleted: false }])
 
+  const autoAnimate = createAutoAnimateDirective()
   return (
     <TabContainer isActive>
       <TodoForm addTodo={addTodo} />
 
-      <div class="flex flex-col gap-1">
+      <div class="flex flex-col gap-1" use:autoAnimate>
         <For each={todos}>{(todo, index) => <TodoItem todo={todo} index={index} />}</For>
       </div>
     </TabContainer>
